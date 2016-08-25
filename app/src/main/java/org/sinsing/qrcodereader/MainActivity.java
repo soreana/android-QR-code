99fc540a2a9a3488bf8b0606047ec5fc49378bb1
soreana <esterlinkof@gmail.com>
2016-08-25 20:12:06 +0430

    read qr code from camera.

--- app/src/main/java/org/sinsing/qrcodereader/MainActivity.java
+++ app/src/main/java/org/sinsing/qrcodereader/MainActivity.java
@@ -9,10 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
@@ -24,44 +29,65 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+
+        final SurfaceView cameraView = (SurfaceView)findViewById(R.id.camera_view);
+        final TextView barcodeInfo = (TextView)findViewById(R.id.code_info);
+
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        final CameraSource cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
             }
         });
 
-        try {
-            Bitmap myQRCode = BitmapFactory.decodeStream(
-                    getAssets().open("myqrcode.jpeg")
-            );
-            BarcodeDetector barcodeDetector =
-                    new BarcodeDetector.Builder(this)
-                            .setBarcodeFormats(Barcode.QR_CODE)
-                            .build();
-            Frame myFrame = new Frame.Builder()
-                    .setBitmap(myQRCode)
-                    .build();
-
-            SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
-
-            if (barcodes.size() != 0) {
-
-                // Print the QR code's message
-                Log.d("My QR Code's Data",
-                        barcodes.valueAt(0).displayValue
-                );
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    assert barcodeInfo != null;
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+
+
     }
 
     @Override
