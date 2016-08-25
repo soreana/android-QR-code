0b62e64247480d08cdbbcd1ff0c6294c0aa47d19
soreana <esterlinkof@gmail.com>
2016-08-25 19:51:01 +0430

    fist commit "after create qr code reader."

---
+++ app/src/main/java/org/sinsing/qrcodereader/MainActivity.java
@@ -0,0 +1,88 @@
+package org.sinsing.qrcodereader;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        try {
+            Bitmap myQRCode = BitmapFactory.decodeStream(
+                    getAssets().open("myqrcode.jpeg")
+            );
+            BarcodeDetector barcodeDetector =
+                    new BarcodeDetector.Builder(this)
+                            .setBarcodeFormats(Barcode.QR_CODE)
+                            .build();
+            Frame myFrame = new Frame.Builder()
+                    .setBitmap(myQRCode)
+                    .build();
+
+            SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
+
+            if (barcodes.size() != 0) {
+
+                // Print the QR code's message
+                Log.d("My QR Code's Data",
+                        barcodes.valueAt(0).displayValue
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
